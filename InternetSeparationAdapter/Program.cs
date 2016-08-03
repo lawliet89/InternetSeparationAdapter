aaf7f4259b308519ea04fb4be91aae7dd4df0c49
Yong Wen Chua <ida@yongwen.xyz>
2016-08-03 18:37:01 +0800

    Use Google API

--- InternetSeparationAdapter/Program.cs
+++ InternetSeparationAdapter/Program.cs
@@ -1,260 +1,92 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using MailKit;
-using MailKit.Net.Imap;
 
 namespace InternetSeparationAdapter
 {
   class Program
   {
-    public static void Main(string[] args)
+    // If modifying these scopes, delete your previously saved credentials
+    // at ~/.credentials/gmail-dotnet-quickstart.json
+    static string[] Scopes = {GmailService.Scope.GmailReadonly};
+    static string ApplicationName = "Gmail API .NET Quickstart";
+
+    static void Main(string[] args)
     {
-      using (var client = new ImapClient(new ProtocolLogger(Console.OpenStandardError())))
+      UserCredential credential;
+
+      using (var stream =
+        new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
       {
-        client.Connect("imap.gmail.com", 993, true);
+        string credPath = System.Environment.GetFolderPath(
+          System.Environment.SpecialFolder.Personal);
+        credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
 
-        // Remove the XOAUTH2 authentication mechanism since we don't have an OAuth2 token.
-        client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-        client.Authenticate(args[0], args[1]);
-
-        client.Inbox.Open(FolderAccess.ReadOnly);
-
-        // Get the summary information of all of the messages (suitable for displaying in a message list).
-        var messages = client.Inbox.Fetch(0, -1, MessageSummaryItems.Full | MessageSummaryItems.UniqueId).ToList();
-
-        // Keep track of messages being expunged so that when the CountChanged event fires, we can tell if it's
-        // because new messages have arrived vs messages being removed (or some combination of the two).
-        client.Inbox.MessageExpunged += (sender, e) =>
-        {
-          var folder = (ImapFolder) sender;
-
-          if (e.Index < messages.Count)
-          {
-            var message = messages[e.Index];
-
-            Console.WriteLine($"{folder}: expunged message {e.Index}: Subject: {message.Envelope.Subject}");
-
-            // Note: If you are keeping a local cache of message information
-            // (e.g. MessageSummary data) for the folder, then you'll need
-            // to remove the message at e.Index.
-            messages.RemoveAt(e.Index);
-          }
-          else
-          {
-            Console.WriteLine($"{folder}: expunged message {e.Index}: Unknown message.");
-          }
-        };
-
-        // Keep track of changes to the number of messages in the folder (this is how we'll tell if new messages have arrived).
-        client.Inbox.CountChanged += (sender, e) =>
-        {
-          // Note: the CountChanged event will fire when new messages arrive in the folder and/or when messages are expunged.
-          var folder = (ImapFolder) sender;
-
-          Console.WriteLine($"The number of messages in {folder} has changed.");
-
-          // Note: because we are keeping track of the MessageExpunged event and updating our
-          // 'messages' list, we know that if we get a CountChanged event and folder.Count is
-          // larger than messages.Count, then it means that new messages have arrived.
-          if (folder.Count > messages.Count)
-          {
-            Console.WriteLine($"{folder.Count - messages.Count} new messages have arrived.");
-
-            // Note: your first instict may be to fetch these new messages now, but you cannot do
-            // that in an event handler (the ImapFolder is not re-entrant).
-            //
-            // If this code had access to the 'done' CancellationTokenSource (see below), it could
-            // cancel that to cause the IDLE loop to end.
-          }
-        };
-
-        // Keep track of flag changes.
-        client.Inbox.MessageFlagsChanged += (sender, e) =>
-        {
-          var folder = (ImapFolder) sender;
-
-          Console.WriteLine($"{folder}: flags for message {e.Index} have changed to: {e.Flags}.");
-        };
-
-
-        using (var exit = new CancellationTokenSource())
-        {
-          var cancellationTask = Task.Run(() =>
-          {
-            Console.WriteLine("Hit any key to end the IDLE loop.");
-            Console.ReadKey();
-            exit.Cancel();
-          });
-
-          while (!exit.IsCancellationRequested)
-          {
-            using (var done = new CancellationTokenSource())
-            {
-              using (exit.Token.Register(() => done.Cancel()))
-              {
-                var task = IdleTask(client, done.Token);
-                task.Wait();
-              }
-            }
-          }
-        }
-
-
-        client.Disconnect(true);
-      }
-    }
-
-    private class IdleState
-    {
-      readonly object mutex = new object();
-      CancellationTokenSource timeout;
-
-      /// <summary>
-      /// Get the cancellation token.
-      /// </summary>
-      /// <remarks>
-      /// <para>The cancellation token is the brute-force approach to cancelling the IDLE and/or NOOP command.</para>
-      /// <para>Using the cancellation token will typically drop the connection to the server and so should
-      /// not be used unless the client is in the process of shutting down or otherwise needs to
-      /// immediately abort communication with the server.</para>
-      /// </remarks>
-      /// <value>The cancellation token.</value>
-      public CancellationToken CancellationToken { get; private set; }
-
-      /// <summary>
-      /// Get the done token.
-      /// </summary>
-      /// <remarks>
-      /// <para>The done token tells the <see cref="Program.IdleTask"/> that the user has requested to end the loop.</para>
-      /// <para>When the done token is cancelled, the <see cref="Program.IdleTask"/> will gracefully come to an end by
-      /// cancelling the timeout and then breaking out of the loop.</para>
-      /// </remarks>
-      /// <value>The done token.</value>
-      public CancellationToken DoneToken { get; private set; }
-
-      /// <summary>
-      /// Get the IMAP client.
-      /// </summary>
-      /// <value>The IMAP client.</value>
-      public ImapClient Client { get; private set; }
-
-      /// <summary>
-      /// Check whether or not either of the CancellationToken's have been cancelled.
-      /// </summary>
-      /// <value><c>true</c> if cancellation was requested; otherwise, <c>false</c>.</value>
-      public bool IsCancellationRequested
-        => CancellationToken.IsCancellationRequested || DoneToken.IsCancellationRequested;
-
-      /// <summary>
-      /// Initializes a new instance of the <see cref="IdleState"/> class.
-      /// </summary>
-      /// <param name="client">The IMAP client.</param>
-      /// <param name="doneToken">The user-controlled 'done' token.</param>
-      /// <param name="cancellationToken">The brute-force cancellation token.</param>
-      public IdleState(ImapClient client, CancellationToken doneToken,
-        CancellationToken cancellationToken = default(CancellationToken))
-      {
-        CancellationToken = cancellationToken;
-        DoneToken = doneToken;
-        Client = client;
-
-        // When the user hits a key, end the current timeout as well
-        doneToken.Register(CancelTimeout);
+        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+          GoogleClientSecrets.Load(stream).Secrets,
+          Scopes,
+          "user",
+          CancellationToken.None,
+          new FileDataStore(credPath, true)).Result;
+        Console.WriteLine("Credential file saved to: " + credPath);
       }
 
-      /// <summary>
-      /// Cancel the timeout token source, forcing ImapClient.Idle() to gracefully exit.
-      /// </summary>
-      void CancelTimeout()
+      // Create Gmail API service.
+      var service = new GmailService(new BaseClientService.Initializer()
       {
-        lock (mutex)
-        {
-          timeout?.Cancel();
-        }
-      }
-
-      /// <summary>
-      /// Set the timeout source.
-      /// </summary>
-      /// <param name="source">The timeout source.</param>
-      public void SetTimeoutSource(CancellationTokenSource source)
-      {
-        lock (mutex)
-        {
-          timeout = source;
-
-          if (timeout != null && IsCancellationRequested)
-            timeout.Cancel();
-        }
-      }
-    }
-
-    static Task IdleTask(ImapClient client, CancellationToken doneToken,
-      CancellationToken cancellationToken = default(CancellationToken))
-    {
-      var idle = new IdleState(client, doneToken, cancellationToken);
-      return Task.Run(() =>
-      {
-        lock (idle.Client.SyncRoot)
-        {
-          // Note: since the IMAP server will drop the connection after 30 minutes, we must loop sending IDLE commands that
-          // last ~29 minutes or until the user has requested that they do not want to IDLE anymore.
-          //
-          // For GMail, we use a 9 minute interval because they do not seem to keep the connection alive for more than ~10 minutes.
-          while (!idle.IsCancellationRequested)
-          {
-            using (var timeout = new CancellationTokenSource(new TimeSpan(0, 0, 10)))
-            {
-              try
-              {
-                // We set the timeout source so that if the idle.DoneToken is cancelled, it can cancel the timeout
-                idle.SetTimeoutSource(timeout);
-
-                if (idle.Client.Capabilities.HasFlag(ImapCapabilities.Idle))
-                {
-                  // The Idle() method will not return until the timeout has elapsed or idle.CancellationToken is cancelled
-                  idle.Client.Idle(timeout.Token, idle.CancellationToken);
-                }
-                else
-                {
-                  // The IMAP server does not support IDLE, so send a NOOP command instead
-                  idle.Client.NoOp(idle.CancellationToken);
-
-                  // Wait for the timeout to elapse or the cancellation token to be cancelled
-                  WaitHandle.WaitAny(new[] {timeout.Token.WaitHandle, idle.CancellationToken.WaitHandle});
-                }
-              }
-              catch (OperationCanceledException)
-              {
-                // This means that idle.CancellationToken was cancelled, not the DoneToken nor the timeout.
-                break;
-              }
-              catch (ImapProtocolException)
-              {
-                // The IMAP server sent garbage in a response and the ImapClient was unable to deal with it.
-                // This should never happen in practice, but it's probably still a good idea to handle it.
-                //
-                // Note: an ImapProtocolException almost always results in the ImapClient getting disconnected.
-                break;
-              }
-              catch (ImapCommandException)
-              {
-                // The IMAP server responded with "NO" or "BAD" to either the IDLE command or the NOOP command.
-                // This should never happen... but again, we're catching it for the sake of completeness.
-                break;
-              }
-              finally
-              {
-                // We're about to Dispose() the timeout source, so set it to null.
-                idle.SetTimeoutSource(null);
-              }
-            }
-          }
-        }
+        HttpClientInitializer = credential,
+        ApplicationName = ApplicationName,
       });
+
+      // Define parameters of request.
+      UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+      // List labels.
+      var labels = request.Execute().Labels;
+      Console.WriteLine("Labels:");
+      if (labels != null && labels.Count > 0)
+      {
+        foreach (var labelItem in labels)
+        {
+          Console.WriteLine("{0}", labelItem.Name);
+        }
+      }
+      else
+      {
+        Console.WriteLine("No labels found.");
+      }
+
+      // Get unread Inbox Messages
+      var inboxUnreadRequest = service.Users.Messages.List("me");
+      inboxUnreadRequest.LabelIds = "INBOX";
+      inboxUnreadRequest.Q = "is:unread";
+
+      var messages = inboxUnreadRequest.Execute().Messages;
+      Console.WriteLine($"{messages.Count} unread");
+
+      foreach (var messageMeta in messages)
+      {
+        Console.WriteLine($"{messageMeta.Id}");
+        var id = messageMeta.Id;
+        var messageRequest = service.Users.Messages.Get("me", id);
+        var message = new Message(messageRequest.Execute());
+        Console.WriteLine($"From: {message.From}");
+        Console.WriteLine($"Subject: {message.Subject}");
+        var body = message.Body;
+        if (body.HasValue)
+        {
+          Console.WriteLine($"Body: {body.Value.Value}");
+        }
+      }
     }
   }
 }
